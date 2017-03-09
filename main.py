ffe6c0330174aad6e92b03c8f14c7f9789623734
Muzza <murster972@yahoo.com>
2017-03-09 00:22:08 +0000

    commit

--- main.py
+++ main.py
@@ -13,23 +13,7 @@ class Trivium:
         self.IV = IV
 
     def encrypt(self):
-        self.A = [0 for x in range(93)]
-        self.B = [0 for x in range(84)]
-        self.C = [0 for x in range(111)]
-
-        if len(self.IV) != 80 or len([x for x in self.IV if x in "10"]) != len(self.IV):
-            raise IVErrorException("An 80-bit IV is required, represented as a binary string, i.e '010101...'."\
-                                   "A IV can be generated if the argument 'gen_IV=1' is passed to encrypt method.")
-        if len(self.key) != 80 or len([x for x in self.key if x in "10"]) != len(self.key):
-            raise Exception("Key error.")
-
-        #load IV into registers
-        for i in range(80): self.A[i] = int(self.IV[i])
-        for i in range(80): self.B[i] = int(self.key[i])
-        for i in range(108, 111): self.C[i] = 1
-
-        #warm-up phase, cipher ran 1152 times to ensure keystream is a product of both the IV and key
-        for i in range(1152): self.cycle()
+        self.load_regs()
 
         pt_bin = ""
         for i in self.pt:
@@ -39,9 +23,36 @@ class Trivium:
 
         key_stream = [self.cycle() for x in range(len(pt_bin))]
         ct = "".join([str((key_stream[x] + pt_bin[x]) % 2) for x in range(len(pt_bin))])
-
-        ct = "".join([chr(int(ct[x:x + 7])) for x in range(0, len(ct), 7)])
+        ct = "".join([chr(int(ct[x:x + 7], 2)) for x in range(0, len(ct), 7)])
         return str(base64.b64encode(bytes(ct, "utf-8")))[2:-1]
+
+    def decrypt(self):
+        self.load_regs()
+
+        d = base64.b64decode(self.ct).decode("utf-8")
+        ct = ""
+        for i in d:
+            b = bin(ord(i))[2:]
+            ct += ("0" * (7 - len(b))) + b
+        ct = [int(x) for x in ct]
+
+        key_stream = [self.cycle() for x in range(len(ct))]
+        pt = "".join([str((key_stream[x] + ct[x]) % 2) for x in range(len(ct))])
+        pt = "".join([chr(int(pt[x:x + 7], 2)) for x in range(0, len(pt), 7)])
+        return pt
+
+    def load_regs(self):
+        self.A = [0 for x in range(93)]
+        self.B = [0 for x in range(84)]
+        self.C = [0 for x in range(111)]
+
+        #load IV into registers
+        for i in range(80): self.A[i] = int(self.IV[i])
+        for i in range(80): self.B[i] = int(self.key[i])
+        for i in range(108, 111): self.C[i] = 1
+
+        #warm-up phase, cipher ran 1152 times to ensure keystream is a product of both the IV and key
+        for i in range(1152): self.cycle()
 
     def cycle(self):
         a_out = (self.A[92] + self.A[65] + (self.A[90] & self.A[91])) % 2
@@ -63,22 +74,6 @@ class Trivium:
 
         return out
 
-    def decrypt(self):
-        self.A = [0 for x in range(93)]
-        self.B = [0 for x in range(84)]
-        self.C = [0 for x in range(111)]
-
-        for i in range(80): self.A[i] = int(self.IV[i])
-        for i in range(80): self.B[i] = int(self.key[i])
-        for i in range(108, 111): self.C[i] = 1
-
-        d = base64.b64decode(self.ct).decode("utf-8")
-        decode = ""
-        for i in d:
-            c = bin(ord(i))[2:]
-            decode += c
-        print(decode)
-
     def gen_key_stream(self, size):
         pass
 
@@ -87,7 +82,7 @@ class IVErrorException(Exception):
 
 if __name__ == '__main__':
     pt = "Hello World!, how are you?"
-    ct = "9I6/sdGW87a0ufO2tqfwmJyF8JirrfCYnIXztJip87SKrvO2tpzwm4aY9IyjoeKdtOKrueKtnPO0mLP0j4+Y9IyljvO0mpbRl/SMpYX0j5GH9I+RhvO2tp3ztJqN9Iy0rA=="
+    ct = "cQ5ZXyUtJUlGXDhhFBkcS3hmTg9lf35dTWw="
     IV = "11101011110000100000110001011001111011110010100001110100000011001100010111000000"
     key = "10101000000001011000110101101101110011000000100001101010110101101111001100101001"
     t = Trivium(ct=ct, key=key, IV=IV)
